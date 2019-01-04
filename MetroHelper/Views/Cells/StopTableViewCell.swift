9619168414991a1edf351d96d6557092ca48c0dc
Francis Breidenbach <fbreidenbach@gmail.com>
2019-01-03 18:33:08 -0800

    Create StopTableViewCell - need to fill in.

---
+++ MetroHelper/Views/Cells/StopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StopTableViewCell.swift
+//  MetroHelper
+//
+//  Created by Francis Breidenbach on 1/3/19.
+//  Copyright © 2019 Francis Breidenbach. All rights reserved.
+//
+
+import UIKit
+
+class StopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

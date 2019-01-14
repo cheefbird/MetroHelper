5a082fe9040cfa39aed2bd235cede4988e49f0b7
Francis Breidenbach <fbreidenbach@gmail.com>
2019-01-14 14:44:56 -0800

    Create mocked Predictions.

---
+++ MetroHelper/Helpers/Fixtures/PredictionData.swift
@@ -0,0 +1,24 @@
+//
+//  PredictionData.swift
+//  MetroHelper
+//
+//  Created by Francis Breidenbach on 1/14/19.
+//  Copyright © 2019 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+
+struct PredictionData {
+  let sampleData: [Prediction]
+  
+  init() {
+    let sampleOne = Prediction(seconds: 647, minutes: 10, routeId: 804, runId: "804_1_var0")
+    let sampleTwo = Prediction(seconds: 1105, minutes: 18, routeId: 804, runId: "804_1_var0")
+    let sampleThree = Prediction(seconds: 174, minutes: 2, routeId: 804, runId: "804_0_var0")
+    let sampleFour = Prediction(seconds: 894, minutes: 14, routeId: 804, runId: "804_0_var0")
+    
+    let sample = [sampleOne, sampleTwo, sampleThree, sampleFour]
+    
+    sampleData = sample
+  }
+}

3d8773ccfca288f35f8093866624f00d6d290072
Francis Breidenbach <fbreidenbach@gmail.com>
2019-01-31 19:43:01 -0800

    Update Stop model per API contract

--- MetroHelper/Models/Stop.swift
+++ MetroHelper/Models/Stop.swift
@@ -7,8 +7,20 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct Stop {
+  let latitude: Float
+  let longitude: Float
   var displayName: String
   var id: Int
+  let line: TrainLine
+  
+  init(forLine line: TrainLine, fromJSON json: JSON) {
+    self.latitude = json["latitude"].floatValue
+    self.longitude = json["longitude"].floatValue
+    self.displayName = json["display_name"].stringValue
+    self.id = json["id"].intValue
+    self.line = line
+  }
 }

3c2810974844e7d455076520cfae924f257c9e2b
Francis Breidenbach <fbreidenbach@gmail.com>
2019-02-04 15:10:36 -0800

    Add project file for new protocol (+1 squashed commit) Squashed commits: [02447ec] Create delegate to handle return to StopTable

---
+++ MetroHelper/Protocols/StopModifierDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  StopModifierDelegate.swift
+//  MetroHelper
+//
+//  Created by Francis Breidenbach on 2/4/19.
+//  Copyright © 2019 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+
+protocol StopModifierDelegate: AnyObject  {
+  
+  // MARK: - Properties
+  
+  // MARK: - Methods
+  
+  func addStop(_ stop: Stop)
+}
